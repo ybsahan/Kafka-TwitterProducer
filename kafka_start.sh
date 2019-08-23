c270539b37cbd6f69dbd779bf535b4a5ff90b0b0
Yusuf Bahadır Şahan <33758240+ybsahan@users.noreply.github.com>
2019-08-23 09:53:47 +0300

    Add files via upload

---
+++ kafka_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ..
+cd kafka_2.12-2.3.0/
+kafka-server-start.sh config/server.properties
+
---
+++ zookeeper_start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ..
+cd kafka_2.12-2.3.0/
+
+zookeeper-server-start.sh config/zookeeper.properties
+
+
+
